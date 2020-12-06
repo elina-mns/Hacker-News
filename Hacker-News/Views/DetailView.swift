e2bc33dcf888f1064d6d028a6b2b3325c223e1f7
elina-mns <ellie.mansurova@gmail.com>
2020-12-06 14:17:07 -0500

    move WebView to another file

--- Hacker-News/Views/DetailView.swift
+++ Hacker-News/Views/DetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct DetailView: View {
     
@@ -23,20 +22,4 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-    }
-}
+
